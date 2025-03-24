9d42352cc5535f2b2cba26af247b6ea0376d918d
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-03-24 11:18:46 +0100

    Add Intro to sort

---
+++ Day 52 - intro-to-sorting.js
@@ -0,0 +1,17 @@
+function introTutorial(V, arr) {
+  // Write your code here
+  return arr.indexOf(V);
+}
+
+// A simple function to get the Index of V in the array
+
+// Sample Input
+
+// V=3
+// [1,2,3]
+// output = 2
+
+// Sample input
+// V=4
+// 1 4 5 7 9 12
+// output = 1
