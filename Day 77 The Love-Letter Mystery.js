35f1b78cc8758b5a246b3df35a7ac0488776366d
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-04-17 00:04:08 +0100

    Add my problem solving approach

--- Day 77 The Love-Letter Mystery.js
+++ Day 77 The Love-Letter Mystery.js
@@ -19,3 +19,25 @@ https: function theLoveLetterMystery(s) {
   }
   return count;
 }
+
+/*
+Sample Input
+abc 
+abcba
+abcd
+cba
+Sample output
+2
+0
+4
+2
+*/
+
+/* 
+My thinking approach
+Divide the string and get the firsthalf and secondHalf, ignore the mid letter
+if isEven then it will divid equalkly, else it will divide abd ignore the mid letter
+The check if the firstHalf and secondHalf are equal, if true return 0
+if its not I will loop through the length of firstString and find the value difference between the string index
+return the count of the value difference of each s
+*/
