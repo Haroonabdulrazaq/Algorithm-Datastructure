533f7e2b2a4253f2475044fdaafbf7d16b4f7eed
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-04-26 23:33:43 +0100

    Add Day 87 Add isValid String

---
+++ Day 87 Sherlock and the Valid String.js
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?isFullScreen=true
+function isValid(s) {
+  // Write your code here
+  let trackObj = {};
+  for (let i = 0; i < s.length; i++) {
+    trackObj[s[i]] = (trackObj[s[i]] || 0) + 1;
+  }
+
+  let valueArr = Object.values(trackObj);
+  let freqNum = {};
+
+  for (let i = 0; i < valueArr.length; i++) {
+    freqNum[valueArr[i]] = (freqNum[valueArr[i]] || 0) + 1;
+  }
+  console.log(freqNum);
+  let keys = Object.keys(freqNum).map(Number);
+  if (keys.length == 1) {
+    return 'YES';
+  }
+
+  if (keys.length == 2) {
+    const [key1, key2] = keys;
+
+    if (
+      (freqNum[key1] === 1 && (key1 - 1 === key2 || key1 - 1 === 0)) ||
+      (freqNum[key2] === 1 && (key2 - 1 === key1 || key2 - 1 === 0))
+    ) {
+      return 'YES';
+    }
+  }
+
+  return 'NO';
+}
