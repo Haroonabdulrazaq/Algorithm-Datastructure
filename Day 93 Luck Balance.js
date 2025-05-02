162b2c93001e078ccb557ffca1472f065353b854
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-05-03 00:02:29 +0100

    Add sample input and problem solving approach

--- Day 93 Luck Balance.js
+++ Day 93 Luck Balance.js
@@ -16,3 +16,25 @@ function luckBalance(k, contests) {
   }
   return sum;
 }
+
+/*
+k The number of important contest she can loose
+contests the map between luck value and it's important contest
+n = 6, k = 3
+5 1         contests = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
+2 1
+1 1
+8 1
+10 0
+5 0
+
+
+*/
+
+/*
+Lena wants to maximise her luck. Her luck reduces when she wins the contest, and increases when she looses
+First I ned tp sort by highest Luck value, in order to discard less value but important, when the k value is 0
+Then add up all the unimportant contest
+Then subtract the less value but important contest from the sum
+That way Lena gets the highest possible luck from loosing her contest
+*/
