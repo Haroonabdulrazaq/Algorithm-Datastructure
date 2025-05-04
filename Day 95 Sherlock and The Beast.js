b03f81d5a89bf7b72ebf2cf9ff5c9c900482038b
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-05-04 22:42:39 +0100

    Add problem solving approach to Sherlock and the Beast

--- Day 95 Sherlock and The Beast.js
+++ Day 95 Sherlock and The Beast.js
@@ -14,3 +14,24 @@ function decentNumber(n) {
     console.log(five.repeat(m) + '' + three.repeat(n - m));
   }
 }
+/*
+Sample Input
+1       n = 1 (first test case)
+3       n = 3 (second test case)
+5
+11
+Sample Output
+-1
+555
+33333
+55555533333
+*/
+
+/*
+My problem solving-approach
+We will keep subtracting 5 until m is a multiple of 3
+If it's a multiple of 3 then,
+we need to check if m is less than zero, if it is we return -1
+else we print 5 by that number
+then we print 3 by the n - m times
+*/
