ac373afb11078400059fa0f78b83326d23ad653c
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-05-07 22:29:54 +0100

    Add Sample Input and problem solving appraoch

--- Day 98 Mark and Toys.js
+++ Day 98 Mark and Toys.js
@@ -11,3 +11,17 @@ function maximumToys(prices, k) {
   }
   return count;
 }
+/*
+Sample Input: 
+1 12 5 111 200 1000 10
+
+Sample Output: 4
+*/
+
+/*
+Problem solving approach
+Sort to ge the leats price toy first
+For each iteration of sortedPrice
+We deduct the price of each toy from the budget k
+increase ccount in each deduction
+*/
