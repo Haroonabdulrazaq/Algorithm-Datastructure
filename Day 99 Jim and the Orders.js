0e86bc702964344ca3f981153d51c172c0bad86c
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-05-08 22:40:44 +0100

    Add Day 99 Sample Input and Output

--- Day 99 Jim and the Orders.js
+++ Day 99 Jim and the Orders.js
@@ -11,7 +11,10 @@ function jimOrders(orders) {
 
   return orderTrackArr.sort((a, b) => a[1] - b[1]).map((element) => element[0]);
 }
-
+/* 
+Sample Input: orders = [[8, 1],[4 ,2],[5, 6],[3, 1],[4, 3]];
+Sample Output: [4, 2, 5, 1, 3]
+*/
 /*
 Problem solving aproach
 - The problem is so get the Order in which the order wll be fulfilled based on the order no and Serve time.
