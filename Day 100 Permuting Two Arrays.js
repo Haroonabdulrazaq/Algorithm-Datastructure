c1f35f2a7f8b16c4f54987fd3d40bfeef7d68ff6
Haroon Abdulrazaq <haroonabdulrazaq@gmail.com>
2025-05-09 23:21:16 +0100

    Add Day 100 Permuting Two Arrays

---
+++ Day 100 Permuting Two Arrays.js
@@ -0,0 +1,27 @@
+//https://www.hackerrank.com/challenges/two-arrays/problem?isFullScreen=true
+function twoArrays(k, A, B) {
+  // Write your code here
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] + B[i] < k) {
+      return 'NO';
+    }
+  }
+  return 'YES';
+}
+
+/*
+Sample Input:
+3 10        A[] and B[] size n = 3, k = 10
+2 1 3       A = [2, 1, 3]
+7 8 9       B = [7, 8, 9]
+4 5         A[] and B[] size n = 4, k = 5
+1 2 2 1     A = [1, 2, 2, 1]
+3 3 3 4     B = [3, 3, 3, 4]
+
+Sample Output:
+YES
+NO
+*/
